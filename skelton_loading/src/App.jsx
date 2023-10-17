877128d57ccff2cc1db648648fb034ae3355b105
UDwarakanath <dwarakanathummadisetti@gmail.com>
2023-10-17 21:26:55 +0530

    loading state and some practical daata using axios

--- skelton_loading/src/App.jsx
+++ skelton_loading/src/App.jsx
@@ -1,12 +1,44 @@
-import SkeletonPost from "./skeletons/SkeletonPost"
-
+import { useEffect, useState } from "react";
+import axios from "axios"
+import SkeletonPost from "./skeletons/SkeletonPost";
 
 const App = () => {
-  return <div >
-   {
-    Array.from({length:10}).map(item=><SkeletonPost/>)
-   }
-  </div>
-}
+      const [users, setUsers] = useState([]);
+      const [loading,setLoading] =useState(false)
 
-export default App
+      useEffect(() => {
+        const fetchPosts = async()=>{
+          setLoading(true)
+          try {
+            const res = await axios.get("http://localhost:3500/posts")
+            const data =await res.data
+            setUsers(data)
+            setLoading(false)
+          } catch (error) {
+            console.log(error)
+          }
+        }
+        fetchPosts()
+      },[]);
+
+      const displayUsers = users.map(user=>{
+        const {title,body} = user
+        return <div>
+          <h6>{title}</h6>
+          <p>{body}</p>
+        </div>
+      })
+
+      const displaySkeleton = <div>
+      {Array.from({ length: 10 }).map((item,index) => (
+            <SkeletonPost key={index}/>
+      ))}
+</div>
+
+
+      return<>
+       {loading ? displaySkeleton : displayUsers};
+      </>
+};
+
+export default App;
